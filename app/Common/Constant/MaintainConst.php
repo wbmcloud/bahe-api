34d6f4701156263d0081597fbaaec53cfb9c1574
wangbaoming <wangbaoming@lianjia.com>
2017-10-11 09:01:13 +0800

    fix: 修改停服文案

--- app/Common/Constant/MaintainConst.php
+++ app/Common/Constant/MaintainConst.php
@@ -7,5 +7,5 @@ class MaintainConst
     /**
      * 服务维护文案
      */
-    const SERVER_DOWN_MAINTAIN_TEXT = '为保证游戏的运行稳定和服务质量，游戏正进行停机维护，请关注微信服务号：xlcyqp来了解开服时间及更多资讯。开服后请务必清理游戏进程或重新安装最新的安装包方可正常进行游戏。';
+    const SERVER_DOWN_MAINTAIN_TEXT = '为保证游戏的运行稳定和服务质量，游戏正进行停机维护，请关注微信服务号：xlcyqp来了解开服时间及更多资讯。开服后请务必清理游戏进程以保证获取最新版本更新。';
 }
